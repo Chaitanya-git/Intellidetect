6db234cdfe5fb6f75c541e3c4050038d75719521
Chaitanya-git <chaitanya2.1999@gmail.com>
2016-04-12 20:55:21 +0530

    Improved network accuracy for the MNIST data set, add a momentum constant.

--- main.cpp
+++ main.cpp
@@ -8,32 +8,33 @@ int main(){
     mat Theta1, Theta2, nn_params;
     X.load("/home/chaitanya/Downloads/MNIST-dataset-in-different-formats/data/CSV format/mnist_train.csv");
     X = shuffle(X);
+    //X = X.rows(0,9999);
     Y = X.col(0);
     X = X.cols(1,X.n_cols-1);
     int inputLayerSize = 784;
-    int HiddenLayerSize = 49;
+    int HiddenLayerSize = 100;
     int OutputLayerSize = 10;
     Theta1 = randInitWeights(HiddenLayerSize, inputLayerSize+1);
     Theta2 = randInitWeights(OutputLayerSize,HiddenLayerSize+1);
 
     cout<<"Weights Initialized"<<endl;
-    if(!nn_params.load("parameters.csv"))
+    //if(!nn_params.load("parameters.csv"))
         nn_params = join_vert(vectorise(Theta1),vectorise(Theta2)); //the weights in a more manageable format
     //cout<<X.n_cols<<endl<<X.n_rows<<endl;
 
-    double lambda = 1, alpha = 0.01; //regularization parameter and learning rate
-    int Total = X.n_rows, batch_size = X.n_rows/100;
+    double lambda = 0, alpha = 1, mu = 0.995; //regularization parameter and learning rate and a momentum constant
+    int Total = X.n_rows, batch_size = X.n_rows/10;
     cout<<"\n\tStarting batch training.\n\n";
     umat prediction = (predict(Theta1, Theta2, X)==Y);
     double accuracy = as_scalar(accu(prediction)*100.0/prediction.n_elem);
     cout<<"Prediction Accuracy before training: "<<accuracy<<endl<<endl;
-    for(int k = 0;k<(Total/batch_size); ++k){
+    for(int k = 0;k<Total/batch_size; ++k){
         mat X_batch = X.rows(batch_size*(k),batch_size*(k+1)-1);
         mat Y_batch = Y.rows(batch_size*(k),batch_size*(k+1)-1);
         cout<<"Batch "<<k+1<<endl;
-        for(int i=0; i<10; ++i){
+        for(int i=0; i<50; ++i){
             cout<<"\tIteration "<<i<<endl;
-            nn_params -= backpropogate(nn_params, inputLayerSize, HiddenLayerSize, OutputLayerSize, X_batch, Y_batch, lambda, alpha);
+            nn_params = mu*nn_params - backpropogate(nn_params, inputLayerSize, HiddenLayerSize, OutputLayerSize, X_batch, Y_batch, lambda, alpha);
         }
     }
     Theta1 = reshape(nn_params.rows(0,(inputLayerSize+1)*(HiddenLayerSize)-1),HiddenLayerSize,inputLayerSize+1);
@@ -44,6 +45,7 @@ int main(){
     cout<<"Prediction Accuracy on training set: "<<accuracy;
     cout<<"\n\nUsing test set"<<endl;
     X.load("/home/chaitanya/Downloads/MNIST-dataset-in-different-formats/data/CSV format/mnist_test.csv");
+    //X = X.rows(0,99);
     Y = X.col(0);
     X = X.cols(1,X.n_cols-1);
 
